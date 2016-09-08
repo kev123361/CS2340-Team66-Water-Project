44613f93b2e9cdd2412c7cb639df941fdac654cf
shobhitsrivastava <shobhit@gatech.edu>
2016-09-08 15:51:32 -0400

    Added calc() method for Person 4 - Shobhit

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Shobhit Srivastava
  *  @version 1.1
  */
 public class Person4 {
@@ -31,7 +31,12 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      String returned = "";
+      for (char i : input) {
+        char added = i + 1;
+        returned = returned + added;
+      }
+      return returned;
     }
     
     /**
